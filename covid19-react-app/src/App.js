7cef1eb23d001f96533841205281882b6e4e3283
gurvir-sandhar <gurvir2@pdx.edu>
2020-05-28 16:42:50 -0700

    added header and fetch api endpoint in app.js, also added background and styles to index.css

--- covid19-react-app/src/App.js
+++ covid19-react-app/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*function App() {
+    return (
+        <div className="App">
+            <header>Hello World</header>
+        </div>
+    );
+}
+
+export default App; */
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            stats: [],
+            isLoaded: false,
+        };
+    }
+
+    componentDidMount() {
+        const url = 'https://api.covid19api.com/summary'
+
+        fetch(url)
+        .then(response => response.json())
+        .then((data) => console.log(data))
+        .catch((error) => console.log(error))
+    }
+    render() {
+        return (
+            <div className="App">
+                <header className="header">
+                    <h1>COVID-19 STATISTICS</h1>
+                </header>
+            </div>
+        )
+    }
 }
 
 export default App;
--- covid19-react-app/src/index.js
+++ covid19-react-app/src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  //strictmode is a dev tool, does not affect production build
+  <React.StrictMode>  
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
